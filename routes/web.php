609ad4601d9eb2b2b2744b25b558b137e32a4d40
HuuLocDangYeu <12a5.lehuuloc@gmail.com>
2025-06-23 19:50:46 +0700

    Cài đặt authentication với laravel/ui

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\BorrowLogController;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,3 +11,7 @@ Route::get('/', function () {
 Route::resource('books', BookController::class);
 Route::resource('borrow-logs', BorrowLogController::class);
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
